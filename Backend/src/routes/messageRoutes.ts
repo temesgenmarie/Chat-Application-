ebfa1cd017d8709aafaefdb077003f4010da1947
Temesgen Marie <tommr2323@gmail.com>
2025-10-26 14:52:44 +0300

    contact route and controller updated

--- Backend/src/routes/messageRoutes.ts
+++ Backend/src/routes/messageRoutes.ts
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.get("/", authMiddleware, getMessages);
 
-router.post("/", authMiddleware, sendMessage);
-router.put("/:messageId",authMiddleware,editMessage)
-router.delete("/:messageId",authMiddleware,deleteMessage)
+// router.post("/", authMiddleware, sendMessage);
+// router.put("/:messageId",authMiddleware,editMessage)
+// router.delete("/:messageId",authMiddleware,deleteMessage)
 
 export default router;
